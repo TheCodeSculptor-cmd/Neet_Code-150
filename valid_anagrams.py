7abb00ff42a3b8319d4272e351c629685af6367d
Sanskar Soni <97246753+Sannutan@users.noreply.github.com>
2025-07-21 12:45:33 +0530

    Second Question

---
+++ valid_anagrams.py
@@ -0,0 +1,29 @@
+s = input("Enter first String: ")
+t = input("Enter second String: ")
+
+if len(s) != len(t):
+    print("False")
+else:
+    countS = {}
+    countT = {}
+
+    for ch in range(len(s)):
+        if s[ch] in countS:
+            countS[s[ch]] += 1
+        else:
+            countS[s[ch]] = 1
+
+
+        if t[ch] in countT:
+            countT[t[ch]] += 1
+        else:
+            countT[t[ch]] = 1
+
+        
+    for char in countS:
+        if countS[char] != countT.get(char, 0):
+            print("false")
+            break
+
+    else:
+        print("True")
