589f30289b6fd01ab9a33f10912c7cdf0136c7df
Sanskar Soni <97246753+Sannutan@users.noreply.github.com>
2025-07-26 23:18:21 +0530

    made one change in program

--- palindrome.py
+++ palindrome.py
@@ -1,24 +1,26 @@
-# Input from user
+# Taking input from user 
 word = input("Enter a String: ")
 
-# Convert to lowercase manually (without using.lower)
 lower_word = ''
+
 for char in word:
     if 'A' <= char <= 'Z':
-        lower_word += chr(ord(char) + 32)   # convert uppercase to lowercase
-    else:
+        lower_word += chr(ord(char) + 32)
+    if ('a' <= char <= 'z') or ('0' <= char <= '9'):
         lower_word += char
 
+    
+# Revered String Manually
 
-# Reverse manually 
 reversed_word = ''
 
 for i in range(len(lower_word) -1, -1, -1):
     reversed_word += lower_word[i]
 
-# Check if palindrome
-if lower_word == reversed_word:
-    print("Palindrome!")
-else:
-    print("Not a palindrome!")
 
+# Check if Palindrom 
+
+if lower_word == reversed_word:
+    print("It's a Palindrome!")
+else:
+    print("It's not a palindrome")
